5d51120e892f9adac03408f6a94c7a789cd332dd
Tetsuyuki Kobayashi <tetsu.koba@gmail.com>
2023-07-05 14:56:52 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module example.com/tcp2stdout
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		log.Println("Usage: %s <port>", os.Args[0])
+		os.Exit(1)
+	}
+	port := os.Args[1]
+
+	listener, err := net.Listen("tcp", "0.0.0.0:"+port)
+	if err != nil {
+		log.Println("Failed to open port on "+port+": ", err)
+		os.Exit(1)
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		<-c
+		log.Println("\nReceived SIGINT or SIGTERM, terminating...")
+		listener.Close()
+		os.Exit(0)
+	}()
+
+	log.Println("Listening on 0.0.0.0:" + port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection: ", err)
+			os.Exit(1)
+		}
+
+		handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	_, err := io.Copy(os.Stdout, conn)
+	if err != nil {
+		log.Println("Error reading from connection: ", err)
+		return
+	}
+}
